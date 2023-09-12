70d904ac7879ac86e9f011508518d0191e908111
tanziti <tanzitian3@gmail.com>
2023-09-12 15:14:40 -0400

    frontend setup

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,30 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import csrfFetch, { restoreCSRF } from './store/csrf';
 
+// ... 
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+  window.csrfFetch = csrfFetch;
+}
+
+// ...
+
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+if (sessionStorage.getItem("X-CSRF-Token") === null) {
+  restoreCSRF().then(renderApplication);
+} else {
+  renderApplication();
+}
 const rootReducer = combineReducers({
   session
 });
